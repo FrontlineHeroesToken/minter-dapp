0a3b7df8781cf6f4a3e028af114a226117906c65
FrontlineHeroesToken <99892643+FrontlineHeroesToken@users.noreply.github.com>
2022-04-12 15:05:05 -0500

    code update

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Frontline Heroes NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Frontline Heroes NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Frontline Heroes NFTs are a Chairtiable NFT Collection. Need Help Join Us in the  <a href="https://t.me/frontlineheroestoken"  target="_blank" rel="noopener noreferrer">Frontline Heroes</a> Main Website or Telegram Channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
